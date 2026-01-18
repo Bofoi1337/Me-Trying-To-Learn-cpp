e9e085a4f52553b27bb8711451358db3a365486d
Duke Nguyen <dnguyen7331@gmail.com>
2026-01-18 11:00:36 -0500

    Added Chapter2Summary File

---
+++ Chapter2Final/io.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+int readNumber()
+{
+    std::cout << " Enter an integer: ";
+    int input{};
+    std::cin >> input;
+
+    return input;
+}
+
+void writeAnswer(int value)
+{
+    std::cout << value;
+}
